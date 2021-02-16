0b41f72ebd657f057ac4a832793e386fea94851c
Martin Sauve <msauve@redhat.com>
2021-02-16 16:24:35 -0500

    roles

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -5,10 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import javax.annotation.security.RolesAllowed;
+
 @Path("/hello-resteasy")
 public class GreetingResource {
 
     @GET
+    @RolesAllowed("usager")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
